d2670ef7dbbb533d4410d02b630c74a0da2d3b1d
Mariam-Elhdad <mariam.elhdad5@gmail.com>
2023-01-30 20:21:24 +0200

    added user models

--- horus/users/models.py
+++ horus/users/models.py
@@ -1,26 +1,66 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (
+AbstractBaseUser, 
+BaseUserManager,
+PermissionsMixin,
+)
+from django.db import models
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from django.utils.timezone import now
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, name, email, password=None):
+        if not username :
+            raise TypeError("user manager should has username.")
+        if not email :
+            raise TypeError("user manager should has email.")
+        if not name:
+            raise TypeError("you should add name.")
+        if not password:
+            raise TypeError("you should add password.")   
+
+        user = self.model(email = self.normalize_email(email), name = name, username=username )
+        user.set_password(password)
+        user.save()
+        return user  
+    def create_superuser(self, username, name, email, password=None):
+        if not username :
+            raise TypeError("user manager should has username.")
+        if not email :
+            raise TypeError("user manager should has email.")
+        if not name:
+            raise TypeError("you should add name.")
+        if not password:
+            raise TypeError("you should add password.")   
+
+        user = self.model(email = self.normalize_email(email), name = name, username=username )
+        user.set_password(password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user         
+        
 
 
-class User(AbstractUser):
-    """
-    Default custom user model for horus.
-    If adding fields that need to be filled at user signup,
-    check forms.SignupForm and forms.SocialSignupForms accordingly.
-    """
 
-    #: First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
-    first_name = None  # type: ignore
-    last_name = None  # type: ignore
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(_("Username"), max_length = 100, unique=True) 
+    name = models.CharField(_("Full Name"), max_length = 255)
+    email = models.EmailField(_("Email"), max_length = 255,unique=True) 
+    is_verified = models.BooleanField(_("Is user verified by email"), default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=now)
 
-    def get_absolute_url(self):
-        """Get url for user's detail view.
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email", "name"]
 
-        Returns:
-            str: URL for user detail.
+    objects = UserManager() 
 
-        """
-        return reverse("users:detail", kwargs={"username": self.username})
+    def __str__(self) :
+        return self.username
+
+
+   
