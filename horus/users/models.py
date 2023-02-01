68bc9b17a9bf83c41d70cb12c2f9780763a81df0
Mariam-Elhdad <mariam.elhdad5@gmail.com>
2023-02-01 14:55:25 +0200

    fix linter issues

--- horus/users/models.py
+++ horus/users/models.py
@@ -1,53 +1,56 @@
 from django.contrib.auth.models import (
-AbstractBaseUser, 
-BaseUserManager,
-PermissionsMixin,
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
 )
 from django.db import models
-from django.db.models import CharField
-from django.urls import reverse
-from django.utils.translation import gettext_lazy as _
 from django.utils.timezone import now
+from django.utils.translation import gettext_lazy as _
+
 
 class UserManager(BaseUserManager):
     def create_user(self, username, name, email, password=None):
-        if not username :
+        if not username:
             raise TypeError("user manager should has username.")
-        if not email :
+        if not email:
             raise TypeError("user manager should has email.")
         if not name:
-            raise TypeError("you should add name.")
+            raise TypeError("user manager should has name.")
         if not password:
-            raise TypeError("you should add password.")   
+            raise TypeError("you should add password.")
 
-        user = self.model(email = self.normalize_email(email), name = name, username=username )
+        user = self.model(
+            email=self.normalize_email(email), name=name, username=username
+        )
+
         user.set_password(password)
         user.save()
-        return user  
+        return user
+
     def create_superuser(self, username, name, email, password=None):
-        if not username :
-            raise TypeError("user manager should has username.")
-        if not email :
-            raise TypeError("user manager should has email.")
+        if not username:
+            raise TypeError("you should add username.")
+        if not email:
+            raise TypeError("you should add email.")
         if not name:
             raise TypeError("you should add name.")
         if not password:
-            raise TypeError("you should add password.")   
+            raise TypeError("you should add password.")
 
-        user = self.model(email = self.normalize_email(email), name = name, username=username )
+        user = self.model(
+            email=self.normalize_email(email), name=name, username=username
+        )
         user.set_password(password)
         user.is_superuser = True
         user.is_staff = True
         user.save()
-        return user         
-        
-
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(_("Username"), max_length = 100, unique=True) 
-    name = models.CharField(_("Full Name"), max_length = 255)
-    email = models.EmailField(_("Email"), max_length = 255,unique=True) 
+    username = models.CharField(_("Username"), max_length=100, unique=True)
+    name = models.CharField(_("Full Name"), max_length=255)
+    email = models.EmailField(_("Email"), max_length=255, unique=True)
     is_verified = models.BooleanField(_("Is user verified by email"), default=False)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
@@ -57,10 +60,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = ["email", "name"]
 
-    objects = UserManager() 
+    objects = UserManager()
 
-    def __str__(self) :
+    def __str__(self):
         return self.username
-
-
-   
