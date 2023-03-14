f1082a938d2052435d8a96ed7b3c8ee7f3073e6f
khaled802 <elkotkhaled802@gmail.com>
2023-03-15 00:08:14 +0200

    add the current state of user in upvote and downvote

--- horus/blog/api/serializers.py
+++ horus/blog/api/serializers.py
@@ -1,14 +1,12 @@
-from rest_framework.exceptions import ValidationError
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
-from taggit.serializers import (TagListSerializerField,
-                                TaggitSerializer)
-from horus.blog.models import Love, Post, Comment, Reply, Upvote
-from django.contrib.auth.models import User
+from taggit.serializers import TaggitSerializer, TagListSerializerField
+
+from horus.blog.models import Comment, Downvote, Love, Post, Reply, Upvote
 
 
 class UserTextBaseSerializer(serializers.ModelSerializer):
-    date_posted = serializers.DateTimeField(format='%Y-%d-%m %H:%M:%S', read_only=True)
+    date_posted = serializers.DateTimeField(format="%Y-%d-%m %H:%M:%S", read_only=True)
     id = serializers.IntegerField(read_only=True)
     creator_id = serializers.SerializerMethodField(read_only=True)
 
@@ -19,14 +17,13 @@ class UserTextBaseSerializer(serializers.ModelSerializer):
         creator = obj.creator
         return creator.id
 
-    
 
 class ReplyCreateSerializer(serializers.ModelSerializer):
     comment_id = serializers.IntegerField()
 
     class Meta:
         model = Reply
-        fields = ['body', 'comment_id']
+        fields = ["body", "comment_id"]
 
     # def create(self, validated_data):
     #     comment = get_object_or_404(Comment, pk=validated_data['comment_id'])
@@ -35,11 +32,11 @@ class ReplyCreateSerializer(serializers.ModelSerializer):
     #     instance.save()
     #     return instance
 
-class ReplySerializer(UserTextBaseSerializer):
 
+class ReplySerializer(UserTextBaseSerializer):
     class Meta:
         model = Reply
-        fields = ['id', 'body', 'date_posted', 'creator_id']
+        fields = ["id", "body", "date_posted", "creator_id"]
 
 
 class LoveSerializer(serializers.ModelSerializer):
@@ -48,8 +45,7 @@ class LoveSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Love
-        fields = ['comment_id', 'creator_id']
-
+        fields = ["comment_id", "creator_id"]
 
 
 class CommentCreateSerializer(serializers.ModelSerializer):
@@ -57,72 +53,89 @@ class CommentCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = ['body', 'post_id']
+        fields = ["body", "post_id"]
 
     def create(self, validated_data):
-        post = get_object_or_404(Post, pk=validated_data['post_id'])
-        creator = self.context['request'].user
+        post = get_object_or_404(Post, pk=validated_data["post_id"])
+        creator = self.context["request"].user
         instance = Comment(post=post, creator=creator, **validated_data)
         instance.save()
         return instance
 
-        
 
 class CommentSerializer(UserTextBaseSerializer):
-    
     class Meta:
         model = Comment
-        fields = ['id', 'body', 'date_posted', 'creator_id']
+        fields = ["id", "body", "date_posted", "creator_id"]
 
-
-    
 
 class CommentFullDataSerializer(UserTextBaseSerializer):
     replies = serializers.SerializerMethodField(read_only=True)
     loves = serializers.SerializerMethodField(read_only=True)
-    
+    is_loved = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Comment
-        fields = ['id', 'body', 'date_posted', 'creator_id', 'replies', 'loves']
+        fields = [
+            "id",
+            "body",
+            "date_posted",
+            "creator_id",
+            "is_loved",
+            "replies",
+            "loves",
+        ]
 
-    
     def get_replies(self, obj: Comment):
         replies = obj.comment_reply.all()
         return ReplySerializer(replies, many=True).data
-    
+
     def get_loves(self, obj: Comment):
         return obj.comment_love.count()
-    
+
+    def get_is_loved(self, obj: Comment):
+        love = Love.objects.filter(comment=obj, creator=self.context["request"].user)
+        if love.exists():
+            return True
+        return False
 
 
 class BasePostSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Post
-        fields = ['id', 'title', 'content']
+        fields = ["id", "title", "content"]
 
 
 class PostSerializer(TaggitSerializer, UserTextBaseSerializer):
     id = serializers.IntegerField(read_only=True)
-    date_posted = serializers.DateTimeField(format='%Y-%d-%m', read_only=True)
+    date_posted = serializers.DateTimeField(format="%Y-%d-%m", read_only=True)
     comments_number = serializers.SerializerMethodField(read_only=True)
     comments = serializers.SerializerMethodField(read_only=True)
     tags = TagListSerializerField()
     creator_id = serializers.IntegerField(read_only=True)
+    voting_state = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Post
-        fields = ['id', 'creator_id', 'title', 'content', 'date_posted', 'comments_number', 'upvotes', 'downvotes',
-                    'comments', 'tags']
+        fields = [
+            "id",
+            "creator_id",
+            "title",
+            "content",
+            "date_posted",
+            "comments_number",
+            "upvotes",
+            "downvotes",
+            "comments",
+            "tags",
+            "voting_state",
+        ]
 
-    
     # def create(self, validated_data):
     #     creator = self.context['request'].user
     #     instance: Post = Post(creator=creator, **validated_data)
     #     instance.save()
     #     return instance
-
-    
 
     def get_comments_number(self, obj: Post):
         return obj.post_comments.count()
@@ -131,15 +144,27 @@ class PostSerializer(TaggitSerializer, UserTextBaseSerializer):
         comments = obj.post_comments.all()
         return CommentSerializer(comments, many=True).data
 
+    def get_voting_state(self, obj: Post):
+        upvote_state = Upvote.objects.filter(
+            post=obj, voter=self.context["request"].user
+        )
+        if upvote_state.exists():
+            return "Upvoted"
+        downvote_state = Downvote.objects.filter(
+            post=obj, voter=self.context["request"].user
+        )
+        if downvote_state.exists():
+            return "Downvoted"
+
 
 class ABCVotingSerializer(serializers.ModelSerializer):
     post_id = serializers.IntegerField()
 
     class Meta:
         model = Upvote
-        fields = ('post_id',)
-        
-    
+        fields = ("post_id",)
+
+
 class UpVoteSerializer(ABCVotingSerializer):
     pass
 
