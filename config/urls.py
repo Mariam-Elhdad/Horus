0ebbad3f34c3913f61b3e4f59479ce458f010b2e
khaled802 <elkotkhaled802@gmail.com>
2023-03-19 22:34:03 +0200

    profile v1

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
     path("users/", include("horus.users.urls", namespace="users")),
+    path("api/profiles/", include("horus.user_profile.api.urls")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ horus/user_profile/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+from horus.users.models import User
+
+# Create your models here.
+
+
+class ImageUpload(models.Model):
+    image = models.ImageField(upload_to="images")
+
+    def __str__(self) -> str:
+        return str(self.image)
+
+    @classmethod
+    def get_default(cls):
+        return cls.objects.first()
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User, related_name="profile_name", on_delete=models.CASCADE
+    )
+    picture = models.OneToOneField(
+        ImageUpload,
+        related_name="profile_picture",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        db_constraint=False,
+    )
+    date_of_birth = models.DateField()
+    phone = models.CharField(max_length=20)
+    bio = models.TextField(default="I am a user")
+    code_country = models.CharField(max_length=5)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def email(self):
+        return self.user.email
+
+    @property
+    def username(self):
+        return self.user.username
+
+    @property
+    def full_name(self):
+        return self.user.name
+
+    def __str__(self) -> str:
+        return self.user.email
