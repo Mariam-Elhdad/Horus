4ec3a839188a01cd04e2da756974d7673e3854bb
khaled802 <elkotkhaled802@gmail.com>
2023-06-23 06:02:20 +0200

    remove interested

--- horus/events/admin.py
+++ horus/events/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Event, EventImage, Going, Interested
+from .models import Event, EventImage, Going
 
 # Register your models here.
 
@@ -12,4 +12,3 @@ class EventAdmin(admin.ModelAdmin):
 admin.site.register(Event, EventAdmin)
 admin.site.register(EventImage)
 admin.site.register(Going)
-admin.site.register(Interested)
