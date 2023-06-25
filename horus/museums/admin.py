75f19d7baed41c3802133517cb3617a18d769d55
khaled802 <elkotkhaled802@gmail.com>
2023-06-25 22:05:51 +0200

    add artifacts and search by category for artifacts

--- horus/museums/admin.py
+++ horus/museums/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Museum
+from .models import Artifact, Museum
 
 
 # Register your models here.
@@ -8,4 +8,9 @@ class MuseumAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
 
 
+class ArtifactAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+
 admin.site.register(Museum, MuseumAdmin)
+admin.site.register(Artifact, ArtifactAdmin)
